8ec6eec438bfe335f2c80fbe698572699eee7d94
Dev-con-fjosueba <fjosueba.dev@gmail.com>
2026-06-24 19:15:59 -0500

    BUBLES agregamos solución reto ValidarCodigo.java

---
+++ src/Bucles/ValidarCodigo.java
@@ -0,0 +1,29 @@
+package Bucles;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidarCodigo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Definimos la restricción de selección
+        String regex="^U\\d{8}$";
+        Pattern pattern = Pattern.compile(regex);
+        boolean esValido = false; // Usamos centinela para el bucle
+        while (!esValido){
+            System.out.println("Digite su codigo: ");
+            String codigo = scanner.nextLine();
+            Matcher matcher = pattern.matcher(codigo);
+            boolean cumple=matcher.matches();
+            // Validamos que el código coincida con las restricciones
+            if (cumple){
+                System.out.println("Código valido. Acceso permitido. ");
+                esValido=true;
+            }else{
+                System.out.println("Formato inválido. Acceso no permitido. Inténtelo nuevamente. ");
+                scanner.nextLine();
+            }
+        }
+    }
+}
